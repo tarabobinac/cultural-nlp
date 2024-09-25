cad0fde1a1e76f980a695048bb81da8c3e496d91
tarabobinac <61031744+tarabobinac@users.noreply.github.com>
2024-09-25 01:26:09 -0400

    Update session.py

--- utils/session.py
+++ utils/session.py
@@ -2,6 +2,7 @@ import streamlit as st
 import openai
 import string
 import random
+from chatbot import intro_response
 
 
 # Generate a 10 characters ID of pattern:
@@ -40,7 +41,16 @@ def session_setup():
         st.session_state['submitted_to_database'] = False
 
     if 'introduction' not in st.session_state:
-        st.session_state['introduction'] = ''
+        st.session_state['introduction'] = intro_response(
+        """Imagine you are a person from the United States moderating a discussion about gun control. Based on 
+        the cultural values from the United States, provide your communication partner with stances why an average 
+        person from the United States would be pro/against gun control. You should provide information in a casual 
+        style in English. Start the conversation with exactly the following prompt and the culturally-relevant 
+        information:\n\n\"Good afternoon. I will be your conversation partner today in a brief discussion about gun 
+        control. This discussion is an opportunity for you to learn about gun control. I want to encourage you to speak 
+        freely. You are not expected to be an expert. Also, no consensus is necessary, you do not need to agree with 
+        the stances I provide. My role is to facilitate your understanding of gun control across cultural viewpoints. 
+        Please start off by telling us something that puzzles you about this topic.\"""")
 
     if 'next_page' not in st.session_state:
         st.session_state['next_page'] = False
